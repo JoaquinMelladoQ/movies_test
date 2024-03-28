3b4128199efc2f482a1667afd03402531e196bdd
Joaquín Mellado Quevedo <mellado.joaquin@gmail.com>
2024-03-27 21:39:16 -0300

    docs: add readme to describe basic setup

--- app/common/components/card/PressableCard.presets.tsx
+++ app/common/components/card/PressableCard.presets.tsx
@@ -2,6 +2,7 @@ import {StyleSheet} from 'react-native';
 
 export const PressableCardComponentStyles = StyleSheet.create({
   pressableContainer: {
+    backgroundColor: '#f3bbff',
     marginVertical: 5,
     alignItems: 'flex-start',
     borderWidth: 5,
--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,3 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset', '@babel/preset-typescript'],
 };
-
-// module.exports = {
-//   presets: [
-//     ['@babel/preset-env', {targets: {node: 'current'}}],
-// '@babel/preset-typescript',
-//   ],
-// };
